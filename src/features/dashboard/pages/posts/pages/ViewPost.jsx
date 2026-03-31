9781cb489e0812c1f761e04acbad6ad27549bca8
diyar-direk <diyardireki111@gmail.com>
2026-04-01 00:11:38 +0300

    post view

--- src/features/dashboard/pages/posts/pages/ViewPost.jsx
+++ src/features/dashboard/pages/posts/pages/ViewPost.jsx
@@ -1,9 +1,17 @@
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import APIClient from "../../../../../utils/ApiClient";
 import { useQuery } from "@tanstack/react-query";
 import endPoints from "../../../../../constant/endPoints";
 import Skeleton from "../../../../../components/skeleton/Skeleton";
 import HandleError from "./../../../../../components/error/HandleError";
+import Breadcrumbs from "./../../../../../components/breadcrumbs/Breadcrumbs";
+import { useTranslation } from "react-i18next";
+import { useMemo } from "react";
+import imgServerSrc from "../../../../../utils/imgServerSrc";
+import { dashboardRouts } from "../../../../../constant/pageRoutes";
+import dateFormatter from "../../../../../utils/dateFormatter";
+import "../style/style.css";
+import "quill/dist/quill.snow.css";
 
 const api = new APIClient(endPoints.posts);
 
@@ -15,11 +23,68 @@ const ViewPost = () => {
     queryFn: () => api.getOne(id),
   });
 
+  const { i18n } = useTranslation();
+  const language = useMemo(() => i18n.language, [i18n.language]);
+
   if (isLoading) return <Skeleton height="400px" />;
 
   if (error) return <HandleError error={error} refetch={refetch} />;
 
-  return <div></div>;
+  return (
+    <>
+      <Breadcrumbs replace={[{ from: id, text: data?.title }]} />
+      <section className="post-view-container">
+        <main className="post-main-info">
+          <h1 className="post-title border-bottom">{data?.title}</h1>
+
+          {data?.excerpt && (
+            <div className="border-bottom">
+              <p className="excerpt"> {data?.excerpt} </p>
+            </div>
+          )}
+
+          <div className="post-info">
+            <Link
+              className="author  link-hover"
+              to={dashboardRouts.author.view(data?.author?.id)}
+            >
+              <article className="author-profile">
+                {data?.author?.full_name?.[0]}
+              </article>
+              {data?.author?.full_name}
+            </Link>
+            <div>
+              <p> content_type </p>
+              <span>{data?.content_type}</span>
+            </div>
+            <div>
+              <p> category </p>
+              <span>{data?.category?.[`name_${language}`]}</span>
+            </div>
+            <div>
+              <p> created_at </p>
+              <span>{dateFormatter(data?.created_at, "fullDate")}</span>
+            </div>
+          </div>
+
+          <div className="cover-image">
+            <img
+              src={imgServerSrc(
+                data?.featured_image || data?.original_post?.featured_image,
+              )}
+              alt=""
+            />
+          </div>
+
+          <div
+            className="ql-editor"
+            dangerouslySetInnerHTML={{ __html: data?.content }}
+          ></div>
+        </main>
+        <aside className="post-sidebar"></aside>
+      </section>
+    </>
+  );
 };
 
 export default ViewPost;
