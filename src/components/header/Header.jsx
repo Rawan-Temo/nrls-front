10f7d2699d1fb2d069a449dd2d04aa2ad2adf264
bashir-mostafa <bashirmostafa@outloo.com>
2026-04-15 13:28:13 +0300

    translating

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,6 @@ import "./style.css";
 import { NavLink } from "react-router";
 import { homeRoutes } from "../../constant/pageRoutes";
 import Search from "./Search";
-<<<<<<< HEAD
 import { useAuth } from "../../context/AuthContext";
 import { mediaTyps, publicationTyps, topicTyps } from "../../constant/enums";
 import NestedMenu from "./NestedMenu";
@@ -10,15 +9,12 @@ import TopHeader from "./TopHeader";
 import IconButton from "./../buttons/IconButton";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useClickOutside } from "../../hooks/useClickOutside";
-=======
 import AuthHelper from "../../utils/authHelper";
 import { useTranslation } from "react-i18next";
->>>>>>> e37cc5a (tran.. v1)
 
 const Header = () => {
   const { user } = useAuth();
 
-<<<<<<< HEAD
   const { isOpen, ref, toggleOpen } = useClickOutside();
 
   return (
@@ -27,37 +23,6 @@ const Header = () => {
 
       <div className="bottom-header container" ref={ref}>
         <IconButton icon={faBars} className="menu" onClick={toggleOpen} />
-=======
-  const { changeMode } = useDarkMode();
-const { t } = useTranslation();
-  return (
-    <>
-      <div className="top-header container">
-        <div>
-          <div className="date">
-            {currentDate}
-            <FontAwesomeIcon icon={faCalendarAlt} />
-          </div>
-          <div className="language">
-            <Tooltip text={t("header.language")} placement="bottom">
-              <span className="current-language">
-                AR <FontAwesomeIcon icon={faChevronDown} />
-              </span>
-            </Tooltip>
-          </div>
-
-          <Tooltip text={t("header.mode")} placement="bottom" onClick={changeMode}>
-            <FontAwesomeIcon icon={faMoon} className="mode" />
-          </Tooltip>
-        </div>
-        <Link className="logo" to={"/"}>
-          <img src={Logo} alt="" />
-        </Link>
-      </div>
-      <div className="bottom-header container">
-        <nav>
-          <NavLink to={"/"}>home</NavLink>
->>>>>>> e37cc5a (tran.. v1)
 
         <nav className={isOpen ? "open" : ""}>
           <NavLink to={"/"} className="link">
@@ -84,7 +49,6 @@ const { t } = useTranslation();
             contact us
           </NavLink>
 
-<<<<<<< HEAD
           {user ? (
             <NavLink to={homeRoutes.dashboard} className="link">
               dashboard
@@ -93,15 +57,6 @@ const { t } = useTranslation();
             <NavLink to={homeRoutes.login} className="link">
               login
             </NavLink>
-=======
-          <NavLink to={homeRoutes.about}>{t("header.about")}</NavLink>
-          <NavLink to={homeRoutes.contact}>{t("header.contact")}</NavLink>
-
-          {isAuthenticated ? (
-            <NavLink to={homeRoutes.dashboard}>{t("header.dashboard")}</NavLink>
-          ) : (
-            <NavLink to={homeRoutes.login}>{t("header.login")}</NavLink>
->>>>>>> e37cc5a (tran.. v1)
           )}
         </nav>
 
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,15 +4,7 @@ export const baseURL = import.meta.env.VITE_BASE_URL;
 
 const axiosInstance = axios.create({
   baseURL,
-<<<<<<< HEAD
   withCredentials: true,
-=======
-  withCredentials: true,  
-  headers: {
-    'Content-Type': 'application/json',
-  },
->>>>>>> e37cc5a (tran.. v1)
 });
-
 
 export default axiosInstance;
